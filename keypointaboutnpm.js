327765813bc453da6e78f27b9e9004c6f8c5023d
suffyan-zafar <66271327+suffyan-zafar@users.noreply.github.com>
2023-02-28 14:05:35 +0500

    package .json

--- keypointaboutnpm.js
+++ keypointaboutnpm.js
@@ -13,3 +13,23 @@
 // npm init   (step by step , press enter to skip) creat package.json file stepby step
 
 // npm init -y  (everthing by default)
+
+
+
+
+//Git
+
+
+
+//not push nodemodules folder to github because it can huge size..
+
+
+//first we clone the code from repo url
+// 2- if we have package json file in code the simply run npm install command. (it can check dependenicies and add node_modules folder)
+
+
+
+
+
+// for DEV-DEPENDENCIESS
+// npm i nodemon (--save-dev  or /D)     adding nodemon devdependencies
